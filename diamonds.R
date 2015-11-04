bfed49d5b8c7d1e51feef328a665ed6a9e85d03b
s-kwak <singang.kwak@gmail.com>
2015-11-04 00:18:01 -0600

    extra cols

--- diamonds.R
+++ diamonds.R
@@ -6,6 +6,7 @@ head(diamonds)
 price_cut <- diamonds %>%
   group_by(cut) %>%
   summarize(avg_depth = mean(depth),
-            avg_price = mean(price))
+            avg_price = mean(price),
+            count_e = length(color[color == 'E']),
+            color_j = length(color[color == 'J']))
 
-View(price_cut)
